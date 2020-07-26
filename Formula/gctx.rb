65e55257d0865e66e26abe0e451cebf5de3ed5b8
Adam Rodger <adam.rodger@gmail.com>
2020-07-26 19:32:04 +0100

    gctx: update to 0.5.0

--- Formula/gctx.rb
+++ Formula/gctx.rb
@@ -1,14 +1,14 @@
 class Gctx < Formula
-    version '0.4.0'
+    version '0.5.0'
     desc "Manage Google Cloud Platform gcloud configurations easily and quickly"
     homepage "https://github.com/adamrodger/gcloud-ctx"
 
     if OS.mac?
         url "https://github.com/adamrodger/gcloud-ctx/releases/download/v#{version}/gctx-x86_64-apple-darwin.tar.gz"
-        sha256 "1688cb1e728c8aef5563ba911b8e6915f62155eb62ecc28142cc34da615d4324"
+        sha256 "129e3e9a88d95eb3eede86f7254178974abfadec44da341596eedd848c7baef0"
     elsif OS.linux?
         url "https://github.com/adamrodger/gcloud-ctx/releases/download/v#{version}/gctx-x86_64-unknown-linux-musl.tar.gz"
-        sha256 "301d502c59c6d6b35424f7f1edc81ee6c64c217ccfd6e31d0e3a8082ea59646f"
+        sha256 "6e999e780c87dc02ef4aac80192881b544789b28343377b9ec63dbeab4b05823"
     end
 
     def install
